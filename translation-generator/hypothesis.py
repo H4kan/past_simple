2ad1d6e057c3cdf3ab91d704e8f50351367dcab6
jszablanowski <jan.szablanowski@gmail.com>
2022-05-19 20:03:43 +0200

    Change matching to alignment

--- translation-generator/hypothesis.py
+++ translation-generator/hypothesis.py
@@ -1,8 +1,8 @@
 class Hypothesis:
-    def __init__(self, words, score, matching, textLength):
-        self.words = words  # eg. [("he", "is"), ("hungry")]
+    def __init__(self, words, score, alignment, textLength):
+        self.words = words  # eg. ["he", "is", "hungry"]
         self.score = score  # pr(P) * pr(E | P)
-        self.matching = matching  # eg. [(1), (3)]
+        self.alignment = alignment  # eg. [1, 2, 3]
         self.textLength = textLength  # eg. 6
 
     # needed for priority queue
