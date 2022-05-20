a5c13594d335fbdeb3f1982c40bae76f2a04d529
H4kan <szymon.sieradzki184@wp.pl>
2022-05-20 16:49:46 +0200

    alg improx

--- constraints.py
+++ constraints.py
@@ -4,5 +4,7 @@ MAX_GEN_WORD = 2;
 MAX_EN_LEN = 75;
 MAX_PL_LEN = 75;
 
+MAX_SHIFT_PROD = 1;
+
 baseFert = [0.1, 0.8, 0.025, 0.025, 0.025, 0.025];
 baseNullFert = [0.8, 0.1, 0.025, 0.025, 0.025, 0.025];
